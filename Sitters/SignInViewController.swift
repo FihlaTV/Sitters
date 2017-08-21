901f18a2d7424358cd691cc1c2ad973afe843ca4
ADENIKE TOMOMEWO <atomomewo1@gmail.com>
2017-08-20 21:39:25 -0700

    signinpage1

--- Sitters/SignInViewController.swift
+++ Sitters/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  Sitters
 //
 //  Created by ADENIKE TOMOMEWO on 8/20/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
